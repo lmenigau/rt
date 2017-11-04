ccb9659274ac2f123b895c1a23ebb6a3c3de836b
Louis MENIGAULT <lmenigau@e1r7p8.42.fr>
2017-11-04 19:29:19 +0100

    foiw

---
+++ main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/04 03:24:28 by lmenigau          #+#    #+#             */
+/*   Updated: 2017/11/04 19:28:36 by lmenigau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Texture *texture;
+	SDL_Event event;
+	SDL_Rect r;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+		return 3;
+	}
+
+	window = SDL_CreateWindow("SDL_CreateTexture",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			1024, 768,
+			SDL_WINDOW_RESIZABLE);
+
+	r.w = 100;
+	r.h = 50;
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
+
+	while (1) {
+		SDL_PollEvent(&event);
+		if(event.type == SDL_QUIT)
+			break;
+		r.x=rand();
+		r.y=rand();
+
+		SDL_SetRenderTarget(renderer, texture);
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+		SDL_RenderClear(renderer);
+		//SDL_RenderDrawRect(renderer,&r);
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
+		for (int i = 10; i < 100; i++)
+			SDL_RenderDrawPoint(renderer, 20, i);
+		//SDL_RenderFillRect(renderer, &r);
+		SDL_SetRenderTarget(renderer, NULL);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	return 0;
+
+}
