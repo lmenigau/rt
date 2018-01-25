9fe27eff772f5e8c04aa63b5f3f09579c04e2ffb
Louis MENIGAULT <lmenigau@e3r6p20.42.fr>
2018-01-25 07:42:43 +0100

    push

--- src/display_ux.c
+++ src/display_ux.c
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 23:00:40 by lmenigau          #+#    #+#             */
-/*   Updated: 2018/01/25 07:28:08 by lmenigau         ###   ########.fr       */
+/*   Updated: 2018/01/25 07:41:55 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 
 void	display_ux(t_context *ctx)
 {
-	float	x, y, cx, cy, ratio, scale;
+	float	x, y,ratio, scale;
 	t_vec3	dir;
 
 	y = 0;
 	ratio = WIDTH / (float)HEIGHT;
-	scale = tanf(M_PI / 8);
+	scale = tanf((float)M_PI / 8);
 	while (y < HEIGHT)
 	{
 		x = 0;
 		while (x < WIDTH )
 		{
 			dir.x = (2 * ((x + 0.5f) / WIDTH) - 1) * ratio * scale;
-			dir.y = (1 - 2 * ((x + 0.5f) / HEIGHT) * scale;
+			dir.y = (1 - 2 * (y + 0.5f) / HEIGHT) * scale;
+			dir.z = -1;
 			x++;
 		}
 		y++;
