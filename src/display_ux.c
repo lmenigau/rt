a5568ae918f27661c8ef01aaa0ba7efef1552776
Louis MENIGAULT <lmenigau@e3r6p20.42.fr>
2018-01-25 07:28:19 +0100

    ray_direction

--- src/display_ux.c
+++ src/display_ux.c
@@ -6,7 +6,7 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 23:00:40 by lmenigau          #+#    #+#             */
-/*   Updated: 2018/01/25 07:23:50 by lmenigau         ###   ########.fr       */
+/*   Updated: 2018/01/25 07:28:08 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,15 @@ void	display_ux(t_context *ctx)
 	t_vec3	dir;
 
 	y = 0;
-	
 	ratio = WIDTH / (float)HEIGHT;
-	scale = tan (M_PI / 4);
+	scale = tanf(M_PI / 8);
 	while (y < HEIGHT)
 	{
 		x = 0;
 		while (x < WIDTH )
 		{
-			dir.x =  (2 * ((x + 0.5f) / WIDTH) - 1) * ratio * scale;
-			dir.y =  (1 - 2 * ((x + 0.5f) / WIDTH) * scale;
+			dir.x = (2 * ((x + 0.5f) / WIDTH) - 1) * ratio * scale;
+			dir.y = (1 - 2 * ((x + 0.5f) / HEIGHT) * scale;
 			x++;
 		}
 		y++;
